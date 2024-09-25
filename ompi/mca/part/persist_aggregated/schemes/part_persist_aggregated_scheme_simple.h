92c25efc2fd00798c214122c662d3e416d247e0d
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-09-25 14:53:13 +0200

    implement simple aggregation scheme

---
+++ ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_simple.h
@@ -0,0 +1,53 @@
+#pragma once // TODO switch to traditional header guards
+
+#include <stdlib.h>
+#include <stdatomic.h>
+#include <stddef.h>
+
+
+// simple and unsafe ring buffer
+struct ring_buffer{
+    size_t capacity;
+    size_t begin;
+    size_t end;
+    char* buffer;
+};
+
+static void ring_buffer_init(struct ring_buffer* rb, size_t count, size_t dtype_size);
+static void ring_buffer_push(struct ring_buffer* rb, void* data, size_t count) ;
+static void ring_buffer_clear(struct ring_buffer* rb);
+static int  ring_buffer_empty(struct ring_buffer* rb);
+
+static void* ring_buffer_pull(struct ring_buffer* rb, size_t count);
+
+static void ring_buffer_free(struct ring_buffer* rb);
+
+
+
+struct part_persist_aggregation_state {
+    // counters for each public partition
+    atomic_int* internal_parts_ready;
+
+    // parameters for message aggregation
+    int aggregation_count;              // how many partitions may be aggregated
+    int internal_partition_count;
+    int public_partition_count;
+
+    // buffer for public partitions ready to send
+    struct ring_buffer public_parts_ready;
+};
+
+int internal_partition(struct part_persist_aggregation_state* state, int public_part);
+
+int first_public_partition(struct part_persist_aggregation_state* state, int internal_part);
+
+void part_persist_aggregate_simple_init(struct part_persist_aggregation_state* state, int internal_partition_count, int public_partition_count);
+
+void part_persist_aggregate_simple_reset(struct part_persist_aggregation_state* state);
+
+void part_persist_aggregate_simple_push(struct part_persist_aggregation_state* state, int partition);
+
+int part_persist_aggregate_simple_pull(struct part_persist_aggregation_state* state);
+
+int part_persist_aggregate_simple_extract(struct part_persist_aggregation_state* state, int public_partition);
+void part_persist_aggregate_simple_free(struct part_persist_aggregation_state* state);
