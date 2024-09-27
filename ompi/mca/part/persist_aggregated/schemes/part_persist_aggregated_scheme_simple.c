bc42cf24645ad75fdc811c232c4c1541ef151303
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-09-27 14:52:37 +0200

    use opal_ring_buffer

--- ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_simple.c
+++ ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_simple.c
@@ -8,55 +8,6 @@
 
 #include <stdio.h>
 
-
-static void ring_buffer_init(struct ring_buffer* rb, size_t count, size_t dtype_size) {
-    rb->capacity = (count + 1) * dtype_size;
-    rb->buffer = (char*) calloc(rb->capacity, sizeof(char));
-    rb->begin = ATOMIC_VAR_INIT(0);
-    rb->end = ATOMIC_VAR_INIT(0);
-    rb->end_internal = ATOMIC_VAR_INIT(0);
-}
-
-
-static void ring_buffer_push(struct ring_buffer* rb, void* data, size_t count) {
-    int offset = atomic_fetch_add(&rb->end_internal, count);        // advance internal write pointer
-    offset = offset % rb->capacity;
-
-    for (int i = 0; i < count; i++) {       // write data (TODO: make this atomic too?)
-        rb->buffer[(offset + i) % rb->capacity] = ((char*)data)[i];
-    }
-
-    atomic_fetch_add(&rb->end, count);      // now data is actually ready to be read
-}
-
-static void* ring_buffer_pull(struct ring_buffer* rb, size_t count) {
-    int index = atomic_fetch_add(&rb->begin, count);
-    return &rb->buffer[index % rb->capacity];
-}
-
-static void ring_buffer_clear(struct ring_buffer* rb) {
-    // reset all indices
-    atomic_fetch_and(&rb->end_internal, 0);
-    atomic_fetch_and(&rb->end, 0);
-    atomic_fetch_and(&rb->begin, 0);
-}
-
-static int ring_buffer_empty(struct ring_buffer* rb) {
-    return atomic_fetch_add(&rb->begin, 0) == atomic_fetch_add(&rb->end, 0);
-}
-
-static int ring_buffer_elements(struct ring_buffer* rb) {
-    return (atomic_fetch_add(&rb->end, 0) - atomic_fetch_add(&rb->begin, 0) + rb->capacity) % rb->capacity;
-}
-
-static void ring_buffer_free(struct ring_buffer* rb) {
-    if (NULL != rb->buffer)
-        free(rb->buffer);
-    rb->buffer = NULL;
-}
-
-
-
 static int internal_partition(struct part_persist_aggregation_state* state, int public_part) {
     return public_part / state->aggregation_count;
 }
@@ -73,48 +24,49 @@ void part_persist_aggregate_simple_init(struct part_persist_aggregation_state* s
     assert(state->aggregation_count > 0);
 
     // initialize counters
-    state->internal_parts_ready = (atomic_int*) calloc(state->internal_partition_count, sizeof(atomic_int));
+    state->internal_parts_ready = (opal_atomic_int32_t*) calloc(state->internal_partition_count, sizeof(atomic_int));
 
     // initialize ring buffer
-    ring_buffer_init(&state->public_parts_ready, state->public_partition_count, sizeof(int));
+    OBJ_CONSTRUCT(&state->public_parts_ready, opal_ring_buffer_t);
+    opal_ring_buffer_init(&state->public_parts_ready, state->public_partition_count + 1);
 }
 
-int part_persist_aggregate_simple_elements(struct part_persist_aggregation_state* state) {
-    return ring_buffer_elements(&state->public_parts_ready);
-}
 
 void part_persist_aggregate_simple_reset(struct part_persist_aggregation_state* state) {
     if (NULL != state->internal_parts_ready) {
         for(int i = 0; i < state->internal_partition_count; i++) {
-            state->internal_parts_ready[i] = ATOMIC_VAR_INIT(0);
+            state->internal_parts_ready[i] = 0;
         }
     }
-    ring_buffer_clear(&state->public_parts_ready);
+
+    // clear ring buffer (should be empty already)
+    while (NULL != opal_ring_buffer_poke(&state->public_parts_ready, 0)) {
+        opal_ring_buffer_pop(&state->public_parts_ready);
+    }
 }
 
 void part_persist_aggregate_simple_push(struct part_persist_aggregation_state* state, int partition) {
     int internal_part = internal_partition(state, partition);
-    int count = atomic_fetch_add(&state->internal_parts_ready[internal_part], 1);
+    int count = opal_atomic_add_fetch_32(&state->internal_parts_ready[internal_part], 1);
 
     // push to buffer if public partition is ready
     if (count == state->aggregation_count - 1) {
-        ring_buffer_push(&state->public_parts_ready, &internal_part, sizeof(int));
+        opal_ring_buffer_push(&state->public_parts_ready, (void*) (internal_part + 1));
     }
 }
 
 int part_persist_aggregate_simple_pull(struct part_persist_aggregation_state* state) {
-    if (ring_buffer_empty(&state->public_parts_ready)) {
+    // check if element can be pulled
+    if (NULL == opal_ring_buffer_poke(&state->public_parts_ready, 0)) {
         return -1;
     }
 
-    int* partition_ptr = (int*)ring_buffer_pull(&state->public_parts_ready, sizeof(int));
-
-    return *partition_ptr;   
+    return ((int)opal_ring_buffer_pop(&state->public_parts_ready)) - 1;
 }
     
 void part_persist_aggregate_simple_free(struct part_persist_aggregation_state* state) {
     if (state->internal_parts_ready != NULL)
         free(state->internal_parts_ready);
     state->internal_parts_ready = NULL;
-    ring_buffer_free(&state->public_parts_ready);
+    OBJ_DESTRUCT(&state->public_parts_ready);
 }
