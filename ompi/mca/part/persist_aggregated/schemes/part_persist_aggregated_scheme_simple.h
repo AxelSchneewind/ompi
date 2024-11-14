7318b13eb33498eb8eb2db4ce758b142d939082c
Axel Schneewind <axel.schneewind@hlrs.de>
2024-11-14 12:39:53 +0100

    rename functions

--- ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_simple.h
+++ ompi/mca/part/persist_aggregated/schemes/part_persist_aggregated_scheme_simple.h
@@ -16,10 +16,6 @@
  * A user-provided partitioning of n partitions of size b can be mapped
  * to an internal partitioning of n/k partitions of size k*b,
  * where k can be selected to optimize internal partition size.
- *
- * This scheme can be used via a queue-like interface, where user-partitions can be pushed
- * and internal partitions can be pulled as soon as all corresponding user-partitions have been
- * inserted.
  */
 
 #ifndef PART_persist_aggregated_scheme_simple_H
@@ -31,12 +27,12 @@
 
 
 /**
- * @brief tracks already inserted public partitions and available internal partitions.
+ * @brief tracks the number of pready calls corresponding to internal partitions
  *
  */
 struct part_persist_aggregation_state {
-    // counters for each public partition
-    opal_atomic_uint32_t *internal_parts_ready;
+    // counters for each internal partition
+    opal_atomic_uint32_t *public_parts_ready;
 
     // parameters for message aggregation
     int aggregation_count; // how many public partitions may be aggregated into an internal one
@@ -48,7 +44,7 @@ struct part_persist_aggregation_state {
 };
 
 /**
- * @brief initializes the aggregation scheme
+ * @brief initializes the aggregation state
  *
  * @param[out] state                        pointer to aggregation state object
  * @param[in] internal_partition_count      number of internal partitions (i.e. number of messages
@@ -63,7 +59,7 @@ OMPI_DECLSPEC void part_persist_aggregate_simple_init(struct part_persist_aggreg
                                                       int last_internal_partition_size);
 
 /**
- * @brief resets the aggregation scheme
+ * @brief resets the aggregation state
  *
  * @param[out] state                pointer to aggregation state object
  */
@@ -71,23 +67,14 @@ OMPI_DECLSPEC void
 part_persist_aggregate_simple_reset(struct part_persist_aggregation_state *state);
 
 /**
- * @brief insert a public partition
+ * @brief marks a public partition as ready
  *
  * @param[in,out] state             pointer to aggregation state object
- * @param[in] partition             index of the public partition to insert
+ * @param[in] partition             index of the public partition to mark ready
  * @param[out] available_partition  index of the internal partition if it is ready, otherwise -1
  */
-OMPI_DECLSPEC void part_persist_aggregate_simple_push(struct part_persist_aggregation_state *state,
+OMPI_DECLSPEC void part_persist_aggregate_simple_pready(struct part_persist_aggregation_state *state,
                                                       int partition, int* available_partition);
-
-/**
- * @brief extracts an internal partition such that all corresponding public partitions have already
- * been inserted
- *
- * @param[in,out] state             pointer to aggregation state object
- * @return                          the index of the public partition, -1 if none available
- */
-OMPI_DECLSPEC int part_persist_aggregate_simple_pull(struct part_persist_aggregation_state *state);
 
 /**
  * @brief destroys the aggregation scheme
