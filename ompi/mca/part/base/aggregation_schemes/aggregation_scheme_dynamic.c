e3399a2523db49f9567cb24ffd9ebe74472e4fb4
Axel Schneewind <axel.schneewind@protonmail.com>
2026-06-03 16:22:45 +0200

    properly implement dynamic aggregation scheme

--- ompi/mca/part/base/aggregation_schemes/aggregation_scheme_dynamic.c
+++ ompi/mca/part/base/aggregation_schemes/aggregation_scheme_dynamic.c
@@ -12,63 +12,124 @@
 
 #include "aggregation_scheme_dynamic.h"
 
+#include "aggregation_scheme_regular.h"
+#include "aggregation_scheme_rb_tree.h"
+#include "aggregation_scheme_interval_tree.h"
+
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 
-void aggregation_scheme_dynamic_psend_init(struct part_persist_aggregation_state *state, aggregation_algorithm algorithm, ...)
+void aggregation_scheme_dynamic_init(struct part_persist_aggregation_state_t *state, aggregation_algorithm algorithm, ...)
 {
     va_list args;
 
     state->algorithm = algorithm;
 
+    // to forward multiple variadic arguments. 
+    // Required as (va_arg(args, int), va_arg(args, int)) has undefined evaluation order
+    int int_args[5];
+
+    // default-initialize function pointers with null
+    state->reset  = NULL;
+    state->free   = NULL;
+    state->pready = NULL;
+    state->pready_range = NULL;
+    state->remaining  = NULL;
+
     void* actual_aggregation_state;
     switch (state->algorithm)
     {
         case INTERVAL_TREE:
+            state->reset  = (unary_fn_ptr)&aggregation_scheme_interval_tree_reset;
+            state->free   = (unary_fn_ptr)&aggregation_scheme_interval_tree_free;
+            state->pready = (pready_fn_ptr)&aggregation_scheme_interval_tree_pready;
+            state->pready_range = (pready_range_fn_ptr)(&aggregation_scheme_interval_tree_pready_range);
+
             va_start(args, 1);
-            actual_aggregation_state = malloc(sizeof(part_persist_interval_tree_aggregation_state));
-            part_persist_interval_tree_aggregation_state_init(actual_aggregation_state, va_arg(args, int));
+            actual_aggregation_state = malloc(sizeof(struct part_persist_interval_tree_aggregation_state_t));
+            aggregation_scheme_interval_tree_init((struct part_persist_interval_tree_aggregation_state_t*)actual_aggregation_state, va_arg(args, int));
             va_end(args);
             break;
         case RB_TREE:
+            state->reset  = (unary_fn_ptr)&aggregation_scheme_rb_tree_reset;
+            state->free   = (unary_fn_ptr)&aggregation_scheme_rb_tree_free;
+            state->pready = (pready_fn_ptr)&aggregation_scheme_rb_tree_pready;
+            state->pready_range = (pready_range_fn_ptr)(&aggregation_scheme_rb_tree_pready_range);
+
             va_start(args, 1);
-            actual_aggregation_state = malloc(sizeof(part_persist_rb_tree_aggregation_state));
-            part_persist_rb_tree_aggregation_state_init(actual_aggregation_state, va_arg(args, int));
+            actual_aggregation_state = malloc(sizeof(struct part_persist_rb_tree_aggregation_state_t));
+            aggregation_scheme_rb_tree_init((struct part_persist_rb_tree_aggregation_state_t*)actual_aggregation_state, va_arg(args, int));
             va_end(args);
             break;
-        case RB_TREE:
+        case REGULAR:
+            state->reset  = (unary_fn_ptr)&aggregation_scheme_regular_reset;
+            state->free   = (unary_fn_ptr)&aggregation_scheme_regular_free;
+            state->pready = (pready_fn_ptr)&aggregation_scheme_regular_pready;
+            state->pready_range = NULL;
+
             va_start(args, 3);
-            actual_aggregation_state = malloc(sizeof(part_persist_rb_tree_aggregation_state));
-            part_persist_rb_tree_aggregation_state_init(actual_aggregation_state, va_arg(args, int), va_arg(args, int), va_arg(args, int));
+            actual_aggregation_state = malloc(sizeof(struct part_persist_regular_aggregation_state_t));
+            int_args[0] = va_arg(args, int);
+            int_args[1] = va_arg(args, int);
+            int_args[2] = va_arg(args, int);
+            aggregation_scheme_regular_init((struct part_persist_regular_aggregation_state_t*)actual_aggregation_state, int_args[0], int_args[1], int_args[2]);
             va_end(args);
             break;
     }
     state->aggregation_state = actual_aggregation_state;
 }
 
-void aggregation_scheme_dynamic_reset(struct part_persist_aggregation_state *state)
+void aggregation_scheme_dynamic_reset(struct part_persist_aggregation_state_t *state)
 {
-    switch (state->algorithm)
+    state->reset(state);
+}
+
+void aggregation_scheme_dynamic_pready(struct part_persist_aggregation_state_t *state, int partition, int* available_partition_min, int* available_partition_max)
+{
+    state->pready(state, partition, available_partition_min, available_partition_max);
+}
+
+void aggregation_scheme_dynamic_pready_range(struct part_persist_aggregation_state_t *state,
+                                       int partition_min, int partition_max, int* available_partition_min, int* available_partition_max)
+{
+    if (NULL != state->pready_range)
     {
-        case INTERVAL_TREE:
-            part_persist_interval_tree_aggregation_state_reset(state->aggregation_state);
-            break;
-        case RB_TREE:
-            part_persist_rb_aggregation_state_reset(state->aggregation_state);
-            break;
-        case RB_TREE:
-            // TODO
-            break;
+        state->pready_range(state, partition_min, partition_max, available_partition_min, available_partition_max);
+    }
+    else 
+    {
+        int some_available = 0;
+        for (size_t partition = partition_min; partition <= partition_max; partition++)
+        {
+            int out_min, out_max;
+            state->pready(state, partition, &out_min, &out_max);
+            if (out_min <= out_max)
+            { // set or grow output interval
+                if (!some_available || out_min < *available_partition_min)
+                    *available_partition_min = out_min;
+                if (!some_available || out_max > *available_partition_max)
+                    *available_partition_max = out_max;
+                some_available = 1;
+            }
+        }
     }
 }
 
-void aggregation_scheme_dynamic_pready(struct part_persist_aggregation_state *state, int partition, int* available_partition_min, int* available_partition_max)
+void aggregation_scheme_dynamic_free(struct part_persist_aggregation_state_t *state)
 {
-
+    state->free(state);
 }
 
-void aggregation_scheme_dynamic_free(struct part_persist_aggregation_state *state)
+void aggregation_scheme_dynamic_remaining(struct part_persist_aggregation_state_t *state, void **remaining, size_t *remaining_count)
 {
-
+    if (NULL != state->remaining)
+    {
+        state->remaining(state, remaining, remaining_count);
+    }
+    else
+    {
+        *remaining = NULL;
+        *remaining_count = 0;
+    }
 }
--- ompi/mca/part/base/aggregation_schemes/aggregation_scheme_dynamic.h
+++ ompi/mca/part/base/aggregation_schemes/aggregation_scheme_dynamic.h
@@ -26,20 +26,32 @@
 #include "opal/include/opal/sys/atomic.h"
 
 
-enum aggregation_algorithm
+typedef enum 
 {
     REGULAR,
     INTERVAL_TREE,
     RB_TREE
-};
+} aggregation_algorithm;
+
+typedef void (*unary_fn_ptr)(void*); // function type
+typedef void (*pready_fn_ptr)(void*, int, int*, int*); // function type
+typedef void (*pready_range_fn_ptr)(void*, int, int, int*, int*); // function type
+
+typedef void (*remaining_fn_ptr)(void*, void** remaining, size_t* remaining_count);
 
 /**
  * @brief tracks the number of pready calls corresponding to internal partitions
  *
  */
-struct part_persist_aggregation_state {
+struct part_persist_aggregation_state_t {
     aggregation_algorithm algorithm;
     void* aggregation_state;
+
+    unary_fn_ptr reset;
+    unary_fn_ptr free;
+    pready_fn_ptr pready;
+    pready_range_fn_ptr pready_range;
+    remaining_fn_ptr remaining;
 };
 
 /**
@@ -50,30 +62,52 @@ struct part_persist_aggregation_state {
  * @param[in] algorithm                     algorithm to use
  * @param[in] factor                        number of public partitions corresponding to a transfer
  */
-void aggregation_scheme_dynamic_psend_init(struct part_persist_aggregation_state *state, aggregation_algorithm algorithm, int factor)
+void aggregation_scheme_dynamic_init(struct part_persist_aggregation_state_t *state, aggregation_algorithm algorithm, ...);
 
 /**
  * @brief resets the aggregation state
  *
  * @param[out] state                pointer to aggregation state object
  */
-void aggregation_scheme_dynamic_reset(struct part_persist_aggregation_state *state);
+void aggregation_scheme_dynamic_reset(struct part_persist_aggregation_state_t *state);
 
 /**
  * @brief marks a public partition as ready
  *
  * @param[in,out] state             pointer to aggregation state object
  * @param[in] partition             index of the public partition to mark ready
- * @param[out] available_partition  index of the internal partition if it is ready, otherwise -1
+ * @param[out] available_partition_min  index of the first internal partition if it is ready
+ * @param[out] available_partition_max  index of the last internal partition if it is ready
  */
-void aggregation_scheme_dynamic_pready(struct part_persist_aggregation_state *state,
-                                       int partition, int* available_partition_min, int* available_partition_last);
+void aggregation_scheme_dynamic_pready(struct part_persist_aggregation_state_t *state,
+                                       int partition, int* available_partition_min, int* available_partition_max);
+
+/**
+ * @brief marks public partitions as ready
+ *
+ * @param[in,out] state             pointer to aggregation state object
+ * @param[in] partition_min             index of the first public partition to mark ready
+ * @param[in] partition_min             index of the last public partition to mark ready
+ * @param[out] available_partition_min  index of the first internal partition if it is ready
+ * @param[out] available_partition_max  index of the last internal partition if it is ready
+ */
+void aggregation_scheme_dynamic_pready_range(struct part_persist_aggregation_state_t *state,
+                                       int partition_min, int partition_max, int* available_partition_min, int* available_partition_max);
 
 /**
  * @brief destroys the aggregation scheme
  *
  * @param[in,out] state             pointer to aggregation state object
  */
-void aggregation_scheme_dynamic_free(struct part_persist_aggregation_state *state);
+void aggregation_scheme_dynamic_free(struct part_persist_aggregation_state_t *state);
+
+/**
+ * @brief obtains all partitions that have not been consumed yet
+ *
+ * @param[in,out] state             pointer to aggregation state object
+ * @param[out] remaining            pointer to the interval_state objects of remaining intervals
+ * @param[out] remaining_count      number of remaining intervals
+ */
+void aggregation_scheme_dynamic_remaining(struct part_persist_aggregation_state_t *state, void** remaining, size_t *remaining_count);
 
 #endif
